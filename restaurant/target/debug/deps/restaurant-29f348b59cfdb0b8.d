380436a0532841d0e63ae1e5e87ceaa83ea16786
Weten-Ogen <alphaoware@gmail.com>
2023-10-04 11:50:00 +0000

    worked on crates

---
+++ restaurant/target/debug/deps/restaurant-29f348b59cfdb0b8.d
@@ -0,0 +1,6 @@
+/home/marcus/Documents/Rust-Journey/restaurant/target/debug/deps/restaurant-29f348b59cfdb0b8.rmeta: src/lib.rs src/front_of_house.rs
+
+/home/marcus/Documents/Rust-Journey/restaurant/target/debug/deps/restaurant-29f348b59cfdb0b8.d: src/lib.rs src/front_of_house.rs
+
+src/lib.rs:
+src/front_of_house.rs:
---
+++ restaurant/target/debug/deps/restaurant-d5d03f38b8bc3639.d
@@ -0,0 +1,6 @@
+/home/marcus/Documents/Rust-Journey/restaurant/target/debug/deps/restaurant-d5d03f38b8bc3639.rmeta: src/lib.rs src/front_of_house.rs
+
+/home/marcus/Documents/Rust-Journey/restaurant/target/debug/deps/restaurant-d5d03f38b8bc3639.d: src/lib.rs src/front_of_house.rs
+
+src/lib.rs:
+src/front_of_house.rs:
